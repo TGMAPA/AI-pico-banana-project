b7b037e212ad8db9533d893ebe3cfa1906c1d8ea
TGMAPA <miguelperez020704@gmail.com>
2025-11-30 19:37:08 -0600

    Update: TimeEmbedding, ResNet, SelfAttention

---
+++ dev/src/model/Unet/Blocks/DownBlock.py
@@ -0,0 +1,17 @@
+from src.config.libraries import *
+from src.config.config import DEVICE
+
+"""
+
+"""
+
+class DownBlock(nn.Module):
+    def __init__(self, in_):
+        super().__init__()
+        
+        
+
+
+    def forward(self, x):
+
+        pass
---
+++ dev/src/model/Unet/Blocks/UpBlock.py
@@ -0,0 +1,17 @@
+from src.config.libraries import *
+from src.config.config import DEVICE
+
+"""
+
+"""
+
+class UpBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+
+
+    def forward(self, x):
+
+        pass
