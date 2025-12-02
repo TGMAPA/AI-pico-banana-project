e0fb465dbf7f0ebe33a417076c11efde9d1527b5
TGMAPA <miguelperez020704@gmail.com>
2025-12-01 19:06:37 -0600

    Server updated, new training reload with patience 15 and 0.95 data

--- dev/src/model/resume_from_checkpoint.py
+++ dev/src/model/resume_from_checkpoint.py
@@ -8,17 +8,36 @@ from src.config.libraries import *
 def main():
      # Create model
     model = PicoBanana(
-        batch_size = 8,
-        num_workers = 16,
-        train_proportion = 0.8,
-        val_proportion = 0.8
+        batch_size = 4,
+        num_workers = 32,
+        train_proportion = 0.95,
+        val_proportion = 0.95
     )
 
-    #model.load_from_checkpoint("src/model/ModelCheckpoints/best_model.ckpt")
+    start = time.time()
+
+    model.load_from_checkpoint("src/model/ModelCheckpoints/best_model.ckpt")
 
     model.load_model(MODEL_SERIALIZED_PATH)
+    
+    print("Model was succesfully loaded...")
 
-    print("loaded...")
+    model.train(
+        epochs = 200,
+        learning_rate = 1e-4
+    )
+
+    end = time.time()
+
+    exec_time_seconds = end - start
+    exec_time_minutes = exec_time_seconds/60
+    exec_time_hrs = exec_time_minutes/60
+
+    print("Executión time  | seconds: ", exec_time_seconds, " | minutes: ", exec_time_minutes, " | hrs: ", exec_time_hrs)
+
+    # Save model
+    model.save_model(serialized_object_path_destination = MODEL_SERIALIZED_PATH)
+    print("Model was saved in : ", MODEL_SERIALIZED_PATH)    
 
 
 if __name__ == "__main__":
